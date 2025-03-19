4b7412d43e58952d85c7f6e7bc9b03ff4fb5c7a6
erikbehrami <154816038+erikbehrami@users.noreply.github.com>
2025-03-19 17:25:45 +0100

    Update HelloApplication.java

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,9 @@
 package com.example.demo1;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
