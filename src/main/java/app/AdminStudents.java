8157ae05cc06a72bbc1149212dc04acdf7c9b848
erikbehrami <154816038+erikbehrami@users.noreply.github.com>
2025-03-23 01:50:24 +0100

    Created AdminStudents

---
+++ src/main/java/app/AdminStudents.java
@@ -0,0 +1,21 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminStudents extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/adminStudents.fxml"));
+
+            stage.setTitle("Admin");
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.show();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
