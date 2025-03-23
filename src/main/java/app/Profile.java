9fa8472b3348ec8098f397dbac647a9829e2d6e6
Enkel Berisha <99924910+enkelberisha@users.noreply.github.com>
2025-03-23 01:31:58 +0100

    Profile rework

--- src/main/java/app/Profile.java
+++ src/main/java/app/Profile.java
@@ -5,11 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class StudentHome extends Application {
+public class Profile extends Application {
     public void start(Stage stage){
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/studentHome.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/profile.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
+            scene.getStylesheets().add(getClass().getResource("/css/profile.css").toExternalForm());
             stage.setTitle("Student Home");
             stage.setScene(scene);
             stage.show();
