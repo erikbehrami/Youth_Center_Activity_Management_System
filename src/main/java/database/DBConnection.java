7557835ed31fae04e5c639d0ece73296794fc743
Enkel Berisha <99924910+enkelberisha@users.noreply.github.com>
2025-03-25 13:04:03 +0100

    database.sql created

---
+++ src/main/java/database/DBConnection.java
@@ -0,0 +1,27 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        String dbUrl = "jdbc:postgresql://localhost/youthCenterDB";
+        String user = "java";
+        String password = "123";
+
+        if(connection != null){
+            return connection;
+        }
+        try {
+            connection= DriverManager.getConnection(dbUrl, user, password);
+        }catch (SQLException e) {
+e.printStackTrace();
+        }
+        return connection;
+
+    }}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens app to javafx.fxml;
     exports app;
     opens controllers to javafx.fxml;
     exports controllers;
+    opens database to javafx.fxml;
+    exports database;
 }
