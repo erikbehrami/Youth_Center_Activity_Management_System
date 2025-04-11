c5535dc655ee86e368f3fadbc81019e9d1449d2f
Vessa-is <islami.vessa@gmail.com>
2025-04-12 00:52:03 +0200

    validity, getinstance +constructor

--- src/main/java/model/Professors.java
+++ src/main/java/model/Professors.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class Professors {
@@ -16,7 +18,9 @@ public class Professors {
     private String gender;
     private String biographicalInfo;
 
-    private Professors(int id, String username, String password, String name, String surname, String email, Date birthdate, String phoneNumber, String address, String gender, String biographicalInfo) {
+    private Professors(int id, String username, String password, String name, String surname,
+                       String email, Date birthdate, String phoneNumber, String address,
+                       String gender, String biographicalInfo, boolean verified) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -28,53 +32,77 @@ public class Professors {
         this.address = address;
         this.gender = gender;
         this.biographicalInfo = biographicalInfo;
+        this.verified = verified;
+    }
 
+    public static Professors getInstance(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String username = resultSet.getString("username");
+        String password = resultSet.getString("password");
+        String name = resultSet.getString("name");
+        String surname = resultSet.getString("surname");
+        String email = resultSet.getString("email");
+        Date birthdate = resultSet.getDate("birthdate");
+        String phoneNumber = resultSet.getString("phoneNumber");
+        String address = resultSet.getString("address");
+        String gender = resultSet.getString("gender");
+        String biographicalInfo = resultSet.getString("biographicalInfo");
+        boolean verified = resultSet.getBoolean("verified");
+
+        return new Professors(id, username, password, name, surname, email,
+                birthdate, phoneNumber, address, gender,
+                biographicalInfo, verified);
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
+
     public boolean isVerified() {
-        return this.verified;
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public String getSurname() {
-        return this.surname;
+        return surname;
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
 
     public Date getBirthdate() {
-        return this.birthdate;
+        return birthdate;
     }
 
     public String getPhoneNumber() {
-        return this.phoneNumber;
+        return phoneNumber;
     }
 
     public String getAddress() {
-        return this.address;
+        return address;
     }
 
     public String getGender() {
-        return this.gender;
+        return gender;
     }
 
     public String getBiographicalInfo() {
-        return this.biographicalInfo;
+        return biographicalInfo;
     }
 }
