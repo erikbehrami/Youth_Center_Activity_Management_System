885f91194f82998f2a74f5e2d6845af33fa6ec9b
Vessa-is <islami.vessa@gmail.com>
2025-04-28 01:27:33 +0200

    Create professors.dto

---
+++ src/main/java/model/dto/professors/CreateProfessorDto.java
@@ -0,0 +1,132 @@
+package model.dto.professors;
+
+import java.sql.Date;
+
+public class CreateProfessorDto {
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+    private String email;
+    private Date birthdate;
+    private String phoneNumber;
+    private String address;
+    private String gender;
+    private String biographicalInfo;
+    private String department;
+    private String academicTitle;
+
+    public CreateProfessorDto(String username, String password, String name, String surname, String email,
+                              Date birthdate, String phoneNumber, String address, String gender,
+                              String biographicalInfo, String department, String academicTitle) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.gender = gender;
+        this.biographicalInfo = biographicalInfo;
+        this.department = department;
+        this.academicTitle = academicTitle;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBiographicalInfo() {
+        return biographicalInfo;
+    }
+
+    public void setBiographicalInfo(String biographicalInfo) {
+        this.biographicalInfo = biographicalInfo;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getAcademicTitle() {
+        return academicTitle;
+    }
+
+    public void setAcademicTitle(String academicTitle) {
+        this.academicTitle = academicTitle;
+    }
+}
